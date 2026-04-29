6d74a3bf856541f00fc7eab2c58333b284c4831d
Rafaella Sant'Anna <rafaella.santanna2010@gmail.com>
2026-04-29 15:15:18 -0300

    Add files via upload

---
+++ Web/ValidacaoDeNumero/script.js
@@ -0,0 +1,30 @@
+function ValidarNumero() {
+    let numero = document.getElementById("numero").value;
+    let mensagem = document.getElementById("mensagem");
+
+    if (numero == "") {
+        mensagem.textContent = "Por favor, insira um número.";
+        mensagem.style.color = "red";
+
+    } else {
+        numero = parseInt(numero);
+
+        if (numero > 10) {
+
+            mensagem.textContent = "O número é maior que 10.";
+            mensagem.style.color = "green";
+
+        } else {    
+            if (numero > 5) {
+
+                mensagem.textContent = "O número é maior que 5, mas menor ou igual a 10.";
+                mensagem.style.color = "#eb8305";
+
+            } else {
+
+                mensagem.textContent = "O número é 5 ou menor.";
+                mensagem.style.color = "blue";
+            }
+        }
+    }
+}
